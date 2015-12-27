1c3192d294d98c4a7c227abdd57518d606706437
Jisse Reitsma <info@yireo.com>
2015-12-27 21:07:57 +0100

    Pending changes

---
+++ source/app/code/community/Yireo/SalesBlock/Lib/Exception/SalesDeniedException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Yireo SalesBlock
+ *
+ * @author Yireo
+ * @package SalesBlock
+ * @copyright Copyright 2015
+ * @license Open Source License (OSL v3)
+ * @link http://www.yireo.com
+ */
+
+class Yireo_SalesBlock_Lib_Exception_SalesDeniedException extends Exception
+{
+}
---
+++ source/app/code/community/Yireo/SalesBlock/Model/Backend/Source/Custompage.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * SalesBlock plugin for Magento
+ *
+ * @package     Yireo_SalesBlock
+ * @author      Yireo (http://www.yireo.com/)
+ * @copyright   Copyright 2015 Yireo (http://www.yireo.com/)
+ * @license     Open Source License (OSL v3)
+ */
+
+class Yireo_SalesBlock_Model_Backend_Source_Custompage
+{
+    protected $_options;
+
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        if (!$this->_options) {
+            $this->_options = Mage::getResourceModel('cms/page_collection')
+                ->load()->toOptionIdArray();
+    
+            array_unshift($this->_options, array('value' => '', 'label'=> Mage::helper('salesblock')->__('(No CMS Page, use custom page instead)')));
+        }
+
+        return $this->_options;
+    }
+
+}
